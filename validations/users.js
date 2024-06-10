42353d17d6c3edbf2b6196a319eb8fbeb14934ca
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-10 15:01:21 +0200

    created users register validation

---
+++ validations/users.js
@@ -0,0 +1,55 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const registerBody = {
+    email: {
+        in: ["body"],
+        notEmpty: {
+            errorMessage: 'L\'email è obbligatoria',
+            bail: true
+        },
+        isEmail: {
+            errorMessage: 'LEmail non valida',
+            bail: true
+        },
+        custom: {
+            option: async (value) => {
+                const user = await prisma.user.findUnique({
+                    where: { email: value }
+                });
+
+                if (user) throw new Error('Esista già un utente con questa email');
+
+                return true;
+            }
+        }
+    },
+    name: {
+        in: ["body"],
+        isString: {
+            errorMessage: 'Il nome non può contenere solo da numeri',
+            bail: true
+        },
+        isLength: {
+            errorMessage: 'Il nome deve contenere almeno 3 caratteri',
+            options: { min: 3 }
+        }
+    },
+    password: {
+        in: ["body"],
+        notEmpty: {
+            errorMessage: 'La password è obbligatoria',
+            bail: true
+        },
+        isString: {
+            errorMessage: 'La password non può contenere solo da numeri',
+            bail: true
+        },
+        isLength: {
+            errorMessage: 'La password deve contenere almeno 8 caratteri',
+            options: { min: 8 }
+        }
+    }
+}
+
+module.exports = { registerBody }
