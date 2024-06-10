2d0a17e10e8281618159c5fd7f6c5a93aa1cd88d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-10 15:01:33 +0200

    added user register validation

--- routers/auth.js
+++ routers/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.js");
+const validator = require('../middlewares/validator.js');
+const { registerBody } = require("../validations/users.js");
 
 // Registrazione
-router.post('/register', authController.register);
+router.post('/register', validator(registerBody), authController.register);
 
 module.exports = router;
