fbb06df6a5bb46fe5f60a45de7fcb68e11d2cf4f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-10 18:13:34 +0200

    added verify ownership

--- routers/posts.js
+++ routers/posts.js
@@ -5,6 +5,7 @@ const validator = require('../middlewares/validator.js');
 const { validationSlug } = require("../validations/generalValidation.js");
 const bodyData = require("../validations/posts.js");
 const validationToken = require("../middlewares/auth.js");
+const verifyOwnership = require("../middlewares/authUser.js");
 
 // Store con validatori (token e dati ricevuti)
 router.post('/', [validationToken, validator(bodyData)], postsController.store);
@@ -15,8 +16,8 @@ router.use('/:slug', validator(validationSlug));
 // Show
 router.get('/:slug', postsController.show);
 // Update con validatori (token e dati ricevuti)
-router.put('/:slug', [validationToken, validator(bodyData)], postsController.update);
+router.put('/:slug', [validationToken, verifyOwnership, validator(bodyData)], postsController.update);
 // Delete con validatore del token
-router.delete('/:slug', validationToken, postsController.destroy);
+router.delete('/:slug', [validationToken, verifyOwnership], postsController.destroy);
 
 module.exports = router;
