305709b3deaf8dcd3b7e937383949f9595ae8322
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-06-10 16:26:59 +0200

    added validation token into store, update and delete

--- routers/posts.js
+++ routers/posts.js
@@ -4,18 +4,19 @@ const postsController = require("../controllers/posts.js");
 const validator = require('../middlewares/validator.js');
 const { validationSlug } = require("../validations/generalValidation.js");
 const bodyData = require("../validations/posts.js");
+const validationToken = require("../middlewares/auth.js");
 
-// Store con validatore
-router.post('/', validator(bodyData), postsController.store);
+// Store con validatori (token e dati ricevuti)
+router.post('/', [validationToken, validator(bodyData)], postsController.store);
 // Index
 router.get('/', postsController.index);
 // Validatore dello slug
 router.use('/:slug', validator(validationSlug));
 // Show
 router.get('/:slug', postsController.show);
-// Update con validatore
-router.put('/:slug', validator(bodyData), postsController.update);
-// Delete
-router.delete('/:slug', postsController.destroy);
+// Update con validatori (token e dati ricevuti)
+router.put('/:slug', [validationToken, validator(bodyData)], postsController.update);
+// Delete con validatore del token
+router.delete('/:slug', validationToken, postsController.destroy);
 
 module.exports = router;
